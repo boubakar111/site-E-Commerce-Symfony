dd1e4f47a375bd9aea3a1ae48852ea506ee0ec95
boubakarAkdouche <yamsel1@live.fr>
2021-08-31 00:03:57 +0200

    mise en place du service panier et sont affichage

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -17,18 +17,18 @@ class PanierController extends AbstractController
     /**
      * @Route("/panier", name="panier")
      */
-    public function index(): Response
+    public function index( ): Response
     {
+
         $panier = $this->panierService->getFullPanier();
-        $total = $this->panierService->totalPnier();
+
         //si le panier et vide 
         if(!$panier){
             return $this->redirectToRoute('home');
         }
+
         return $this->render('panier/index.html.twig', [
            'panierProduit'=>$panier ,
-            'total'=>$total
-
         ]);
     }
 
@@ -37,9 +37,10 @@ class PanierController extends AbstractController
      * @Route("/panier/add/{id}" , name ="add_panier")
      */
 
-    public function addPanier($id  ): Response{
+    public function addPanier($id ): Response{
 
         $this->panierService->addToPanier($id);
+
         return $this->redirectToRoute('panier');
 
     }
@@ -50,9 +51,8 @@ class PanierController extends AbstractController
      * @Route("/panier/delete/{id}" , name ="delete_panier")
      */
 
-    public function deleteFromPanier($id): Response{
-
-        $this->panierService->deleteFromPanier($id);
+    public function deleteFromPanier(PanierService $panierService , $id): Response{
+        $panierService->deleteFromPanier($id);
         return $this->redirectToRoute('panier');
     }
 
@@ -61,7 +61,8 @@ class PanierController extends AbstractController
      * @Route("/panier/deleteAll/{id}" , name ="delete_All_panier")
      */
     public function delateAllFromPnier($id){
-        $this->panierService->deleteAllPanier($id);
+
+        $this->panierService->deleteProductFromPanier($id);
         return  $this->redirectToRoute('panier');
     }
 
--- src/Services/PanierService.php
+++ src/Services/PanierService.php
@@ -1,93 +1,70 @@
 <?php
+
 namespace App\Services;
 
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class PanierService{
+class PanierService
+{
     private $session;
     private $repoProduct;
     private $tva = 0.2;
 
-    public function __construct(SessionInterface   $session , ProductRepository  $repoProduct)
+    public function __construct(SessionInterface $session, ProductRepository $repoProduct)
     {
         $this->session = $session;
         $this->repoProduct = $repoProduct;
     }
 
+
     // ajouter un produit au panier
-    public function addToPanier($id){
+    public function addToPanier($id)
+    {
+        // recuperer le panier
         $panier = $this->getPanier();
         //si le produit existe dans le panier en incrimente le produit
-        if(isset($panier[$id])){
+        if (isset($panier[$id])) {
             $panier[$id]++;
-        }else{
+        } else {
             // s'il n'exiiste pas dans le panier en le cree
-            $panier[$id]=1;
+            $panier[$id] = 1;
         }
+        // metre ajour le panier
         $this->updatePanier($panier);
     }
 
-   //metre a jour le pnaier
-    public function updatePanier($panier){
 
-       $this->session->set('panier',$panier);
-       //mettre le panier dans une session
-        $this->session->set('sessionPanier',$this->getFullPanier());
+    //recuprer le panier si non  un tableau vid
+    public function getPanier()
+    {
+        return $this->session->get('panier', []);
     }
 
     //supprimer un produit du panier 
-    public function deleteFromPanier($id){
-        $panier = $this->getPanier();
-         //si le produit est deja dans le panier
-        if(isset($panier[$id])){
-            //si le produit et superieur a un
-            if($panier[$id] > 1){
-                $panier[$id]--;
-            }else{
-                //s'il egale a un en le supprimer du panier
-                unset($panier[$id]);
-            }
-            $this->updatePanier($panier);
-        }
-    }
-// supprimer produit totalemnt  du  pnaier
-    public function deleteAllPanier($id){
-        $panier = $this->getPanier();
-        //si produit existe dans le panier  en le supprimer
-        if(isset($panier[$id])) {
-            unset($panier[$id]);
-            $this->updatePanier($panier);
-        }
 
+    public function updatePanier($panier)
+    {
+        $this->session->set('panier', $panier);
+        $this->session->set('cartData' , $this->getFullPanier());
     }
 
-    //supprimer totalement  tous les produit du panier
-    public function deletePanier(){
-        $this->updatePanier([]);
-    }
 
-    public function  getPanier(){
-        return $this->session->get('panier',[]);
-    }
-
-    //recuper tout le panier  avec les produit et la quantity
+// récupérer la totalité du panier
     public function getFullPanier()
     {
         $panier = $this->getPanier();
-
         $fullPanier = [];
-        foreach ($panier as $id => $quantity )
-        {
+        foreach ($panier as $id => $quantity) {
             $product = $this->repoProduct->find($id);
-            if ($product)
-            {
-                $fullPanier['products'][]=[
-                    'quantity'=>$quantity,
-                    'product'=>$product,
+            if ($product) {
+                $fullPanier[] = [
+                    'quantity' => $quantity,
+                    'product' => $product,
                 ];
 
-            }else{
+            } else {
+                //id incorrecte , porduit n'existe pas
                 $this->deleteFromPanier($id);
             }
 
@@ -95,16 +72,55 @@ class PanierService{
 
         return $fullPanier;
     }
- //total du panier
-     public function totalPnier($panier){
 
-         $total = 0;
-         foreach ($panier as   $produit ){
-             $prix = $produit['product']->getPrix();
-             $total +=  $produit['quantity'] * ($produit['product']->getPrix()/100); ;
+    //supprimer totalement  tous les produit du panier
 
-         };
-         return $total;
-     }
+    public function deleteFromPanier($id)
+    {
+        $panier = $this->getPanier();
+        //si le produit est deja dans le panier
+        if (isset($panier[$id])) {
+            //si le produit existe plus d'une fois
+            if ($panier[$id] > 1) {
+                $panier[$id]--;
+            } else {
+                //s'il egale a un en le supprimer du panier
+                unset($panier[$id]);
+            }
+            $this->updatePanier($panier);
+        }
+    }
+
+
+    public function deleteProductFromPanier($id)
+    {
+        $panier = $this->getPanier();
+        //si produit existe dans le panier  en le supprimer
+        if (isset($panier[$id])) {
+            unset($panier[$id]);
+
+            $this->updatePanier($panier);
+        }
+
+    }
+
+    //recuper tout le panier  avec les produit et la quantity
+    public function deletePanier()
+    {
+        $this->updatePanier([]);
+    }
+
+
+    //total du panier
+    public function totalPnier($panier)
+    {
+        $total = 0;
+        foreach ($panier as $produit) {
+            $prix = $produit['product']->getPrix();
+            $total += $produit['quantity'] * ($produit['product']->getPrix() / 100);;
+        };
+        return $total;
+    }
 }
+
 ?>
